24f8e32076e85df630bee812e4841b87a8ee5721
Wanda Hayes <whayeswsfg@gmail.com>
2026-09-19 20:42:07 -0400

    rag vector and retrieval

---
+++ src/RAG/rag_vector_retriever.py
@@ -0,0 +1,48 @@
+from langchain_docling import DoclingLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+#where the files are
+#FILE_PATH = "C:\\Users\\hayes\\Documents\\whragpdffolder\\RAGPDF\\samplereq.pdf"
+FILE_PATH = "C:\\Users\\hayes\\Documents\\whragpdffolder\\RAGPDF\\RCV_Academy_Onboarding.docx"
+
+ragdoc = (DoclingLoader(file_path=FILE_PATH))
+
+document= ragdoc.load()
+
+#print(document)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+doc_splits = text_splitter.split_documents(document)
+#print("The content of first split is: ", doc_splits[0])
+
+embeddings = OpenAIEmbeddings (
+    model="text-embedding-3-large"
+)
+
+vector_store = InMemoryVectorStore(
+   embeddings
+)
+
+vector_store.add_documents(doc_splits)
+
+#results = vector_store.similarity_search(
+#    query="what is the mandatory training for week 1"
+#)
+
+#for doc in results:
+#    print(doc.page_content)
+
+retriever = vector_store.as_retriever(
+    search_type = "mmr",
+    research_kwargs={"k":6}
+)
+
+results=retriever.invoke("What is the mandatory training for week 2")
+for doc in results:
+    print(doc.page_content)
